5c93959002f8189826c5e3990f4f00f2facdb98b
alexrockt <alex@dfghj.de>
2013-02-14 20:52:23 +0100

    now removing quotes, if first and last character of tweet is a quote

--- extract.py
+++ extract.py
@@ -14,4 +14,6 @@ output = unicodecsv.writer(out, encoding='utf-8')
 output.writerow(["id"]+["timestamp"]+["tweet"])  # header row
 
 for row in data:
+	if row["text"].startswith('"') and row["text"].endswith('"'):
+		row["text"] = row["text"][1:-1]
 	output.writerow([row["id"],row["created_at"],row["text"]])
